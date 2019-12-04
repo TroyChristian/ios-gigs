623200d61d2a7ee4ac24efb7f0afa7ef605649d9
Troy Christian <apostasy8@gmail.com>
2019-12-04 15:46:59 -0500

    Part 1 complete storyboard layed out

---
+++ gigs/gigs/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  gigs
+//
+//  Created by Lambda_School_Loaner_219 on 12/4/19.
+//  Copyright © 2019 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBAction func SignUpButton(_ sender: UIButton) {
+    }
+    @IBAction func loginTypeChanged(_ sender: UISegmentedControl) {
+    }
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
