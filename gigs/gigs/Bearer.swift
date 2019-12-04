79345915ce6e7303d0d898a7b04910d97deeffb6
Troy Christian <apostasy8@gmail.com>
2019-12-04 15:49:27 -0500

    model made

---
+++ gigs/gigs/Bearer.swift
@@ -0,0 +1,12 @@
+//
+//  Bearer.swift
+//  gigs
+//
+//  Created by Lambda_School_Loaner_219 on 12/4/19.
+//  Copyright © 2019 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import Foundation
+struct Bearer: Codable {
+    let token: String
+}
