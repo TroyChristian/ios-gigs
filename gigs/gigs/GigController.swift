a432168f16cbc0e71f7d6982e3139225b4623709
Troy Christian <apostasy8@gmail.com>
2019-12-04 16:38:59 -0500

    Login Function complete in GigController

--- gigs/gigs/GigController.swift
+++ gigs/gigs/GigController.swift
@@ -58,6 +58,60 @@ class GigController{
         }.resume()
     
     /// log in /users/login
+        func login(with user: User, completion: @escaping (Error?) -> Void) {
+            guard let url = baseURL else {return}
+            let loginURL = url.appendingPathComponent("users/login")
+            var request = URLRequest(url:loginURL)
+            request.httpMethod = HTTPMethod.post.rawValue
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let encoder = JSONEncoder()
+            
+            do{
+                let data = try encoder.encode(user)
+                request.httpBody = data
+                
+                
+            } catch let encodeError {
+                print("Error \(encodeError)")
+                completion(encodeError)
+                return
+            }
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print(error)
+                    completion(error)
+                    return
+                }
+                if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                    completion(NSError(domain:"", code: response.statusCode, userInfo:nil))
+                    return
+                
+            }
+                guard let data = data else {
+                    completion(NSError())
+                    return
+                }
+                
+                let decoder = JSONDecoder()
+                do {
+                    self.bearer = try decoder.decode(Bearer.self, from: data)
+                    
+                } catch let decoderError {
+                    print(decoderError)
+                    completion(decoderError)
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+       
+                
+            }.resume()
+            
+        }
     
     
 }
