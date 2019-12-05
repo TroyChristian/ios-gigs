dbe5e263a854dcbf00cf72e8ab1c02b54df32abf
Troy Christian <apostasy8@gmail.com>
2019-12-05 15:51:28 -0500

    Storyboard set up and connected

---
+++ gigs/gigs/GigDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GigDetailViewController.swift
+//  gigs
+//
+//  Created by Lambda_School_Loaner_219 on 12/5/19.
+//  Copyright © 2019 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import UIKit
+
+class GigDetailViewController: UIViewController {
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+       @IBOutlet weak var descriptionTextField: UITextView!
+    override func viewDidLoad() {
+     
+        super.viewDidLoad()
+
+       
+    }
+    
+
+ 
+
+}
